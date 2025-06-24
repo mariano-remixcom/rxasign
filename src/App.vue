24ddd9cdd5528fb2ffd57a46dff768cac562abbe
Federico Marquez <17768182+fefu-marquez@users.noreply.github.com>
2025-06-24 17:08:56 -0300

    chore: import bootstrap js

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,9 @@
 <script>
 import DefaultLayout from './layouts/default/DefaultLayout.vue'
 import Toaster from './composables/alerts/toasts/Toaster.vue'
+
+// eslint-disable-next-line sort-imports-es6-autofix/sort-imports-es6, no-unused-vars
+import * as bootstrap from 'bootstrap'
 import '@/scss/app.scss'
 
 export default {
