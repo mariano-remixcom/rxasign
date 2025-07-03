3ad0772d2d3908b8a48f80190ae5d3d5dae86118
Federico Marquez <17768182+fefu-marquez@users.noreply.github.com>
2025-07-03 10:52:39 -0300

    chore: remove pinia, run audit fix

--- src/main.js
+++ src/main.js
@@ -3,14 +3,12 @@ import Vuelidate from '@vuelidate/core'
 import es from 'moment/dist/locale/es'
 import moment from 'moment/dist/moment'
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import { router } from './router'
 
 moment.updateLocale('es', es)
 
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(router)
 app.use(Vuelidate)
 
